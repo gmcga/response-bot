7371c709daa2bf3987ec87026d041e80e45c46a2
gmcga <gmcgaughey@live.com>
2021-07-29 10:45:20 -0400

    Add misc cog

---
+++ cogs/Misc.py
@@ -0,0 +1,53 @@
+# Cog for misc commands
+import discord
+from discord.ext import commands
+import requests
+import json
+
+ # get random quote function
+def get_quote():
+    response = requests.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q'] + " -" + json_data[0]['a']
+    return(quote)
+
+    # get random dish function
+def get_dish(): 
+    response = requests.get("https://random-data-api.com/api/food/random_food")
+    dish = response.json()["dish"] 
+    return(dish) 
+
+class Misc(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+   
+
+    # hello command 
+    @commands.command(aliases = ['hi'], brief = 'Says hello!', description = 'Says hello!')
+    async def hello(self, ctx):
+        await ctx.send(f"Hello {ctx.author.mention}!")
+
+
+    # deletes user's command and sends the text provided
+    @commands.command(brief = "Deletes your command and sends the text provided.", description = "Makes it appear as if the bot sent the text you provided without a prompt. ")
+    async def echo(self, ctx, *, message=None):
+        message = message or "Please provide the message to be repeated."
+        await ctx.message.delete()
+        await ctx.send(message)
+
+    # says random dish command
+    @commands.command(brief = 'Says a random dish', description = 'Replies with a random dish from https://random-data-api.com/api/food/random_food')
+    async def dish(self, ctx):
+        dish = get_dish()
+        await ctx.send(dish)
+
+    # provides random inspirational quote
+    @commands.command(brief = 'Provides an inspiring quote', description = 'Replies with a random quote from https://zenquotes.io/api/random')
+    async def inspire(self, ctx):
+        quote = get_quote()
+        await ctx.send(quote)
+
+
+def setup(bot):
+    bot.add_cog(Misc(bot))
